dc77e1c832a5f24b9dd46aa188af95d1a76f293f
tkys <tkysyskw@gmail.com>
2023-04-25 18:39:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import  streamlit as st
 import subprocess
 
 
-pip_list = subprocess.run('pip', 'list', capture_output=True, text=True)
+pip_list = subprocess.run(['pip', 'list'] , capture_output=True, text=True)
 st.write("pip_list")
