02b62f0947d7e70a10c878e78e1b3d4785cf065b
tkys <tkysyskw@gmail.com>
2023-04-25 18:48:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import  streamlit as st
 import subprocess
-import sentence_transformers
-
 
 pip_list = subprocess.run(['pip', 'list'] , capture_output=True, text=True)
 st.write(pip_list)
+
+
+
+st.title("sentence_transformers")
+
+         
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+#Our sentences we like to encode
+sentences = ['This framework generates embeddings for each input sentence',
+    'Sentences are passed as a list of string.', 
+    'The quick brown fox jumps over the lazy dog.']
+
+#Sentences are encoded by calling model.encode()
+sentence_embeddings = model.encode(sentences)
+
+#Print the embeddings
+for sentence, embedding in zip(sentences, sentence_embeddings):
+    st.write("Sentence:", sentence)
+    st.write("Embedding:", embedding)
+    st.write("")
