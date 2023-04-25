7b7288077be4e51f5636a3c0900aa750131af6fb
tkys <tkysyskw@gmail.com>
2023-04-25 19:07:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import  streamlit as st
 import subprocess
 
-pip_list = subprocess.run(['pip', 'list'] , capture_output=True, text=True)
-st.write(pip_list)
+command = subprocess.run(['pip', 'list'] , capture_output=True, text=True)
+st.write(command)
 
+command = subprocess.run(['free', '-h'] , capture_output=True, text=True)
+st.write(command)
 
 
 st.title("sentence_transformers")
